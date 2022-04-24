dabde5f4b9ae932ef626c3eb729fa4146f75c497
Michal <michalgrzegorczyk04@gmail.com>
2022-04-24 11:53:35 +0200

    Get tasks function

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(
+    private tasksService: TaskService
+  ) {}
 
   ngOnInit(): void {
+    this.tasks = this.tasksService.getTasks();
   }
-
 }
